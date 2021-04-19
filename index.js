d5eaf2d9393f8e4b5c52ac87b0ecb8b05c245180
axaysushir <akshay.sushir99@gmail.com>
2021-04-19 15:22:04 +0530

    Text NLP

---
+++ index.js
@@ -0,0 +1,24 @@
+var nlp = require('compromise')
+
+var t = nlp('dinasour').nouns().toPlural()
+console.log(t.text())
+
+// .match() - Match text
+let doc = nlp('i went on a talk')
+let m = doc.match('. walk', { fuzzy: 0.7 })
+
+console.log(m.text())
+
+// .verbs() - conjugate and negate verbs in any tense:
+
+let verb = nlp('she sells seashells by the seashore.')
+verb.verbs().toPastTense()
+console.log(verb.text())
+
+// .nouns() - Play between plural. singular and possesive form 
+
+let newNoun = nlp('the yellow car')
+newNoun.nouns().toPlural()
+console.log(newNoun.text());
+
+// .numbers()
