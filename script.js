211dee03d3fcb9371c5121fa5112d100b5a606dd
axaysushir <akshay.sushir99@gmail.com>
2021-05-15 18:33:58 +0530

    Check bug number #814 and reproduce it

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-import compromise from 'compromise'
-import syllables from 'compromise-syllables'
+const nlp = require('compromise')
+nlp.extend(require('compromise-syllables'))
 
-const nlp = compromise.extend(syllables)
 
 const word = 'silent'
 console.log(nlp(word).terms().syllables()[0].syllables);
+
+let json = nlp('Chocolate microscopes silent').terms().syllables();
+console.log((json));
+
+// [
+//     {
+//       text: 'Chocolate',
+//       normal: 'chocolate',
+//       syllables: [ 'cho', 'co', 'late' ]
+//     },
+//     {
+//       text: 'microscopes',
+//       normal: 'microscopes',
+//       syllables: [ 'mic', 'ros', 'copes' ]
+//     },
+//     { text: 'silent', normal: 'silent', syllables: [ 'silent' ] }
+// ]
